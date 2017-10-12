fb6a3b6dd0e4ab115a498bac918e60c0c6ca7b73
godconfidence <13718256454@163.com>
2017-10-12 23:16:17 +0800

    fixed

---
+++ src/YY.Edu.Sys.Api/Controllers/BaseController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace YY.Edu.Sys.Api.Controllers
+{
+    public class BaseController : ApiController
+    {
+
+        /// <summary>
+        /// 获取验证的提示信息
+        /// </summary>
+        public string ValidationMsg
+        {
+            get
+            {
+                string error = "";
+                foreach (var item in ModelState.Values)
+                {
+                    foreach (var sub_item in item.Errors)
+                    {
+                        error += sub_item.ErrorMessage + ",";
+                    }
+                    error = error.TrimEnd(',');
+                    error += "\n";
+                }
+
+                return error;
+            }
+        }
+
+    }
+}
---
+++ src/YY.Edu.Sys.Api/Models/ResponseModel/VenueInfoResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YY.Edu.Sys.Api.Models.ResponseModel
+{
+    public class VenueInfoResponse : Sys.Models.VenueInfo
+    {
+        public string VenueCode { get; set; }
+    }
+}
